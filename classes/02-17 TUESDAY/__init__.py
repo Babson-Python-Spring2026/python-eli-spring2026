3eb653779254d447b0047ccad7a7031de25252a4
student <student@example.com>
2026-02-17 19:43:05 -0500

    Lab 1

---
+++ classes/02-17 TUESDAY/__init__.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    sequence = [0, 1]
+    
+    for i in range(2, n):
+        next_num = sequence[i-1] + sequence[i-2]
+        sequence.append(next_num)
+    
+    return sequence
+
+print(fibonacci(10))
